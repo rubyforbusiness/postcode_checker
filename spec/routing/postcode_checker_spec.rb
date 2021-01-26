39e65b6ac597aff6f2f9d6ffe2a6744ebde8eec3
Richard Brooke <ragnarcom@googlemail.com>
2021-01-26 11:14:58 +0000

    post check

--- spec/routing/postcode_checker_spec.rb
+++ spec/routing/postcode_checker_spec.rb
@@ -3,10 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe 'routing for app' do
-  it 'routes as expected' do
+  it 'get postcode/check routes as expected' do
     expect(get: '/postcodes/check').to route_to(
+                                         controller: 'postcodes',
+                                         action: 'check'
+                                       )
+  end
+  it 'post postcode/check routes as expected' do
+    expect(post: '/postcodes/check').to route_to(
       controller: 'postcodes',
-      action: 'check'
+      action: 'submit'
     )
   end
 end
