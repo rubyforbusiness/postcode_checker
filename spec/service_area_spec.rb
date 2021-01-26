b39c6f3a8bab81840faa6568dce685b979d45287
Richard Brooke <ragnarcom@googlemail.com>
2021-01-26 15:42:18 +0000

    serveable postcode_allowed_list

--- spec/service_area_spec.rb
+++ spec/service_area_spec.rb
@@ -7,12 +7,21 @@ RSpec.describe ServiceArea do
 
   describe '#servable' do
     let(:postcode) { 'ABC 123' }
+    let(:args) { {} }
     subject do
-      described_class.new.servable?(make_postcode(postcode))
+      described_class.new(args).servable?(make_postcode(postcode))
     end
     context 'no lists' do
       it 'is false' do
         expect(subject).to be false
+      end
+    end
+
+    context 'postcode_allowed_list contains postcode' do
+      let(:postcode_allowed_list) { [postcode] }
+      let(:args) { {postcode_allowed_list: postcode_allowed_list} }
+      it 'is true' do
+        expect(subject).to be true
       end
     end
   end
