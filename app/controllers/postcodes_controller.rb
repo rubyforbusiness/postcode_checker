efd101505d98aef1b01b6368a1cdba425d12bfda
Richard Brooke <ragnarcom@googlemail.com>
2021-01-27 16:47:34 +0000

    moved methods from controller to helper

--- app/controllers/postcodes_controller.rb
+++ app/controllers/postcodes_controller.rb
@@ -6,6 +6,8 @@ require 'postcode_allowed_list'
 
 # Main controller for application
 class PostcodesController < ApplicationController
+  helper :postcodes
+
   def check; end
 
   def submit
@@ -13,29 +15,10 @@ class PostcodesController < ApplicationController
     @raw_postcode = permitted[:postcode]
     postcode = Postcode.new(@raw_postcode)
     @message = if postcode.valid?
-                 servable_message(postcode)
+                 helpers.servable_message(postcode)
                else
                  postcode.validation_message
                end
     render :check
-  end
-
-  private
-
-  def servable_message(postcode)
-    case Rails.configuration.service_area.servable?(postcode)
-    when true
-      served_message(@raw_postcode)
-    else
-      not_served_message(@raw_postcode)
-    end
-  end
-
-  def served_message(raw_postcode)
-    "Good news. #{raw_postcode} is in our service area"
-  end
-
-  def not_served_message(raw_postcode)
-    "Sorry #{raw_postcode} is not in our service area"
   end
 end
